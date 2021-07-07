14388bc952ac6b8898c4da3e5ccfd610733ad29e
Rithika F Johnson <82044487+rithikafjohnson@users.noreply.github.com>
2021-07-07 23:31:19 +0530

    UPDATED MentalHealth.javaAND mentalhealth.json

--- Zeta/app/src/main/java/com/example/zeta/MentalHealth.java
+++ Zeta/app/src/main/java/com/example/zeta/MentalHealth.java
@@ -4,11 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 public class MentalHealth extends AppCompatActivity {
+
+    //TextView agencyName, personName, contactNumber, city;
+
+    ArrayList<String> emaillist = new ArrayList<>();
+    ArrayList<String> psychiatristlist = new ArrayList<>();
+    ArrayList<String> numberlist = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mental_health);
+        parseJSON();
+    }
+
+    public void parseJSON(){
+        String json;
+        try{
+            InputStream is=getAssets().open("mentalhealth.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer,"UTF-8");
+            JSONArray jsonArray=new JSONArray(json);
+
+            for(int i=0; i<jsonArray.length(); i++){
+                JSONObject obj=jsonArray.getJSONObject(i);
+
+                if(obj.getString("City").equals("Bangalore")){
+                    numberlist.add(obj.getString("Contact Number"));
+                    psychiatristlist.add(obj.getString("Name of the Psychiatrist"));
+                    emaillist.add(obj.getString("E-Mail Address"));
+                }
+            }
+
+            //Toast.makeText(getApplicationContext(), numberlist.toString(),Toast.LENGTH_LONG).show();
+
+
+        } catch (IOException e){
+            e.printStackTrace();
+        } catch(JSONException e){
+            e.printStackTrace();
+        }
     }
 }
